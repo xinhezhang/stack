365d77ccf39843d8cb4cbe8561153137b1e884c6
xinhezhang <xinhezhang@hotmail.com>
2018-01-15 15:41:47 -0800

    setup redux starter code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 const App = () => {
   return (
-    <View />
+    <Provider store={createStore}>
+      <View />
+    </Provider>
   );
 };
 
