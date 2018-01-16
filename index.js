bd872beb96d23cb676eeefe7868a5d29baa9765e
xinhezhang <xinhezhang@hotmail.com>
2018-01-15 17:42:45 -0800

    get rid of warning

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 
+// get rid of “Remote debugger is in a background tab” warning
+console.ignoredYellowBox = ['Remote debugger'];
+
 AppRegistry.registerComponent('stack', () => App);
+
+// How to add app icon?
+//
+// https://blog.bam.tech/developper-news/change-your-react-native-app-icons-in-a-single-command-line
+//
+// npm install -g yo generator-rn-toolbox
+// brew install imagemagick
+// yo rn-toolbox:assets --icon <path to your icon>
